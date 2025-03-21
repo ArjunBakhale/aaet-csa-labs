c9826963927bbf125e59a349784b67f0287e7f6d
Arjun Bakhale <arjun.bakhale@gmail.com>
2025-03-21 11:38:31 -0400

    arrlist

--- arrayList/src/MyArrayList.java
+++ arrayList/src/MyArrayList.java
@@ -1,20 +1,13 @@
 public class MyArrayList<E> {
-    // Default capacity for internal array
-    private static final int DEFAULT_CAPACITY = 2;
-
-    // Internal array to store elements
+    private static final int DEFAULT_CAPACITY = 10;
     private Object[] elements;
-
-    // Number of elements in the ArrayList
     private int size;
 
-    // Default constructor
     public MyArrayList() {
         elements = new Object[DEFAULT_CAPACITY];
         size = 0;
     }
 
-    // Constructor with initial capacity
     public MyArrayList(int initialCapacity) {
         if (initialCapacity < 0) {
             throw new IllegalArgumentException("Illegal Capacity: " + initialCapacity);
@@ -23,73 +16,133 @@ public class MyArrayList<E> {
         size = 0;
     }
 
-    // Add an element to the end of the list
     public boolean append(E element) {
-        // TODO: Ensure capacity, add element, increment size
+        ensureCapacity(size + 1);
+        elements[size++] = element;
         return true;
     }
 
-    // Insert element at specific index
     public void add(int index, E element) {
-        // TODO: Check index bounds, ensure capacity, shift elements, insert new element
+        if (index > size || index < 0) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+        }
+
+        ensureCapacity(size + 1);
+
+        for (int i = size; i > index; i--) {
+            elements[i] = elements[i - 1];
+        }
+
+        elements[index] = element;
+        size++;
     }
 
-    // Get element at index
     @SuppressWarnings("unchecked")
     public E get(int index) {
-        // TODO: Check index bounds, return element
-        return null;
+        if (index >= size || index < 0) {
+            throw new IndexOutOfBoundsException("Index out of bounds exception");
+        }
+
+        return (E) elements[index];
     }
 
-    // Replace element at index
+    @SuppressWarnings("unchecked")
     public E set(int index, E element) {
-        // TODO: Check index bounds, replace element, return old value
-        return null;
+        if (index >= size || index < 0) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+        }
+
+        E oldValue = (E) elements[index];
+        elements[index] = element;
+        return oldValue;
     }
 
-    // Remove element at index
+    @SuppressWarnings("unchecked")
     public E remove(int index) {
-        // TODO: Check index bounds, remove element, shift elements, return removed element
-        return null;
+        if (index >= size || index < 0) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+        }
+
+        E oldValue = (E) elements[index];
+
+        int numMoved = size - index - 1;
+        if (numMoved > 0) {
+            System.arraycopy(elements, index + 1, elements, index, numMoved);
+        }
+
+        elements[--size] = null;
+        return oldValue;
     }
 
-    // Remove first occurrence of element
     public boolean remove(Object o) {
-        // TODO: Find element, remove it if found
+        if (o == null) {
+            for (int i = 0; i < size; i++) {
+                if (elements[i] == null) {
+                    fastRemove(i);
+                    return true;
+                }
+            }
+        } else {
+            for (int i = 0; i < size; i++) {
+                if (o.equals(elements[i])) {
+                    fastRemove(i);
+                    return true;
+                }
+            }
+        }
         return false;
     }
 
-    // Get number of elements
+    private void fastRemove(int index) {
+        int numMoved = size - index - 1;
+        if (numMoved > 0) {
+            System.arraycopy(elements, index + 1, elements, index, numMoved);
+        }
+        elements[--size] = null;
+    }
+
     public int size() {
         return size;
     }
 
-    // Check if list is empty
     public boolean isEmpty() {
         return size == 0;
     }
 
-    // Clear the list
     public void clear() {
-        // TODO: Clear array, reset size
+        for (int i = 0; i < size; i++) {
+            elements[i] = null;
+        }
+        size = 0;
     }
 
-    // Check if list contains element
     public boolean contains(Object o) {
-        // TODO: Search for element
-        return false;
+        return indexOf(o) >= 0;
     }
 
-    // Find index of first occurrence of element
     public int indexOf(Object o) {
-        // TODO: Find index of element
+        if (o == null) {
+            for (int i = 0; i < size; i++) {
+                if (elements[i] == null) {
+                    return i;
+                }
+            }
+        } else {
+            for (int i = 0; i < size; i++) {
+                if (o.equals(elements[i])) {
+                    return i;
+                }
+            }
+        }
         return -1;
     }
 
-    // Ensure internal array has enough capacity
     private void ensureCapacity(int minCapacity) {
-        // TODO: Resize array if current size is less than minCapacity
-        // The new size will be double the old size
-        // Array elements are copied to the new array
+        if (minCapacity > elements.length) {
+            int newCapacity = Math.max(elements.length * 2, minCapacity);
+            Object[] newElements = new Object[newCapacity];
+            System.arraycopy(elements, 0, newElements, 0, size);
+            elements = newElements;
+        }
     }
 }
